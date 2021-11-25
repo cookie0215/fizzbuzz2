97003dd33bc26da861f1bcc2cf5a65bd23bb95e4
cookie0215 <jina_0215@naver.com>
2021-11-25 15:19:14 +0900

    feat: Print 'buzz' if i is timess of 5
    TODO: Print fizzbuzz if i is times of 15

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,5 @@
 for(let i=1; i<=16; i++){
-    if( i % 3 === 0 ) {
-        console.log(`${i} fizz`);
-    }else if( i % 5 === 0 ) {
-        console.log(`${i} buzz`);
-    }else {
-        console.log(`${i}`);
-    }
+    if( i % 3 === 0 ) console.log(`${i} fizz`);
+    else if( i % 5 === 0 ) console.log(`${i} buzz`);
+    else console.log(`${i}`);
 }
