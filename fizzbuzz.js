701a051d9a4dde854f31131000b72e2c866c2838
cookie0215 <jina_0215@naver.com>
2021-11-25 15:28:12 +0900

    feat: fizzbuzz with if, for done

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,7 @@
 // Do fizzbuzz!
 for(let i=1; i<=16; i++){
-    if( i % 3 === 0 ) console.log(`${i} fizz`);
+    if( (i % 3 === 0) && (i % 5 === 0) ) console.log(`숫자${i} fizzbuzz`);    
+    else if( i % 3 === 0 ) console.log(`${i} fizz`);
     else if( i % 5 === 0 ) console.log(`${i} buzz`);
     else console.log(`${i}`);
 }
