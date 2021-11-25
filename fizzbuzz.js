819dcf5523e526a2aa0865b89a5528b1777d60c9
cookie0215 <jina_0215@naver.com>
2021-11-25 14:39:57 +0900

    feat: create for-if-5

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,9 @@
 for(let i=1; i<=16; i++){
-  if( i % 3 === 0 ) {
-     console.log(`${i} fizz`);
-  }else {
-     console.log(`${i}`);
-  }
+    if( i % 3 === 0 ) {
+        console.log(`${i} fizz`);
+    }else if( i % 5 === 0 ) {
+        console.log(`${i} buzz`);
+    }else {
+        console.log(`${i}`);
+    }
 }
